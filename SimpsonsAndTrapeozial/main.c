042e762cd4898f752d35f9e5dd60da1af2991f0e
Betim Hodza <106614572+Betim-Hodza@users.noreply.github.com>
2024-05-02 12:26:36 -0500

    UPDATE: simpsons rule now works!

--- SimpsonsAndTrapeozial/main.c
+++ SimpsonsAndTrapeozial/main.c
@@ -2,122 +2,149 @@
 #include <math.h>
 #include <ctype.h>
 #include <string.h>
+#include "tinyexpr-master/tinyexpr.h"
 
 #define MAX_FUNC_SIZE 100
 
-double parseFunction(char *function, double x);
+// double parseFunction(char *function, double x);
 double simpsonsApprox(int N, int b, int a, char* function, double deltaX);
 double trapezoidalApprox(int N, int b, int a, char* function, double deltaX);
 
-/*function looks through the string and parses out computational information
-e.g. if the function was x+x^2+5 it would take that and return */
-double parseFunction(char *function, double x)
-{
-    double result = 0.0;
-    double prevValue = 0.0;
-    int prevValSet = 0;
+/* 
+Deprecated function, my attempt at parsing math (just didnt work how i wanted it to)
 
-    char *token, *rest = function;
-    char op = '+';
+function looks through the string and parses out computational information
+e.g. if the function was x+x^2+5 it would take that and return 
+// double parseFunction(char *function, double x)
+// {
+//     double result = 0.0;
+//     double prevValue = 0.0;
+//     int prevValSet = 0;
+//     char op = '+';
 
-    while ((token = strtok_r(rest, "+-", &rest)))
-    {
-        double value = 1.0;
+//     char *token = function;
 
-        if (*token == 'x')
-        {
-            value = x;
-            token++;
-        }
-        else if (isdigit(*token))
-        {
-            value = strtod(token, &token);
-        }
+//     while (*token)
+//     {
+//         double value = 1.0;
 
-        if (*token == '^')
-        {
-            token++;
-            double exponent = strtod(token, &token);
-            value = pow(value, exponent);
-        }
+//         // Skip whitespace characters
+//         while (*token == ' ')
+//             token++;
 
-        switch (op)
-        {
-            case '+':
-                result += value;
-                break;
-            
-            case '-':
-                result -= value;
-                break;
-            
-            case '*':
-                if (prevValSet)
-                {
-                    result = prevValue * value;
-                }
-                else
-                {
-                    result *= value;
-                }
-                break;
+//         if (*token == 'x')
+//         {
+//             value = x;
+//             token++;
+//         }
+//         else if (isdigit(*token))
+//         {
+//             char *endptr;
+//             value = strtod(token, &endptr);
+//             token = endptr;
+//         }
 
-            case '/':
-                if (prevValSet)
-                {
-                    result = prevValue * value;
-                }
-                else
-                {
-                    result /= value;
-                }
-                break;
+//         if (*token == '^')
+//         {
+//             token++;
+//             char *endptr;
+//             double exponent = strtod(token, &endptr);
+//             token = endptr;
+//             prevValue = pow(prevValue, exponent);
+//         }
+//         else
+//         {
+//             switch (op)
+//             {
+//                 case '+':
+//                     result += prevValue;
+//                     prevValue = value;
+//                     break;
+//                 case '-':
+//                     result += prevValue;
+//                     prevValue = -value;
+//                     break;
+//                 case '*':
+//                     prevValue *= value;
+//                     break;
+//                 case '/':
+//                     prevValue /= value;
+//                     break;
+//                 default:
+//                     prevValue = value;
+//                     break;
+//             }
 
-            default:
-                break;
-        }
+//             prevValSet = 1;
+//             op = *token;
+//             token++;
+//         }
+//     }
 
-        prevValue = value;
-        prevValSet = 1;
-        op = (*rest == ' ') ? '+' : *rest;
-    }
-    
-    return result;
-}
+//     result += prevValue;
+
+//     return result;
+// }
+*/
+
+
 
 double simpsonsApprox(int N, int b, int a, char* function, double deltaX)
 {
-    //initialize the sum
-    double sum = 0;
-    
-    //iterate over the intervals
+    double sum = 0.0;
+    double x = a;
+
+    int err;
+    te_variable vars1[] = {{"x", &x}};
+
+    //compile expression 1
+    te_expr *expr1 = te_compile(function, vars1, 1, &err);
+
+    double f_a = te_eval(expr1); //returns expression value
+    x = b;
+
+    te_variable vars2[] = {{"x", &x}};
+    //compile expression 2
+    te_expr *expr2 = te_compile(function, vars2, 1, &err);
+
+    double f_b = te_eval(expr2);
+
+    sum += f_a + f_b; // Add the first and last terms
+
+    free(expr1);
+    free(expr2);
+
     for (int i = 1; i < N; i++)
     {
-        double x = a + i * deltaX;
-        double fX = parseFunction(function, x);
+        x = a + i * deltaX;
+        te_variable vars3[] = {{"x", &x}};
+        //compile expression 2
+        te_expr *expr3 = te_compile(function, vars3, 1, &err);
 
-        //first and last terms we multiply by 1
-        if (i == 1 || i == N)
+        double f_x = te_eval(expr3);
+
+        if (i == N - 1)
         {
-            sum += fX;
+            // First and second-to-last terms are multiplied by 4
+            sum += 4 * f_x;
         }
         else if (i % 2 == 0)
         {
-            //even index terms are multiplied by 2
-            sum += 2 * fX;
+            // Even-indexed terms (except the second-to-last) are multiplied by 2
+            sum += 2 * f_x;
         }
-        else if (i % 2 == 1 || (i == N-1))
+        else
         {
-            //odd index terms are multiplied by 4
-            //the second to last term is multiplied by 4
-            sum += 4 *fX;
+            // Odd-indexed terms (except the first) are multiplied by 4
+            sum += 4 * f_x;
         }
+        free(expr3);
     }
 
-    //combine and return sum
-    sum = (deltaX / 3) * (sum);
-    return sum;
+    // Combine the terms
+    sum = deltaX * sum / 3;
 
+    return sum;
 }
 
 double trapezoidalApprox(int N, int b, int a, char* function, double deltaX)
@@ -139,6 +166,7 @@ int main()
     int N, b, a;
     char function[MAX_FUNC_SIZE];
     char choice;
+    int loop = 1;
     
     //Starter menu
     printMenu();
@@ -146,22 +174,34 @@ int main()
     scanf("%c", &choice);
 
     //error checking on the choice
-    while (1)
+    if (choice == '1' || choice == '2')
     {
-        if (choice == '1' || choice == '2')
+        loop = 0;
+        //grab newline
+        scanf("%c");
+    }
+    else
+    {
+        while (loop)
         {
-            break;
-        }
-        else
-        {
-            printf("Please reinput your choice\n");
-            scanf("%c", &choice);
+            scanf("%c");
+            if (choice == '1' || choice == '2')
+            {
+                loop = 0;
+                break;
+            }
+            else
+            {
+                printf("Please reinput your choice\n");
+                scanf("%c", &choice);
+            }
         }
     }
+    
 
-    printf("Please input the function you want to integrate (max chars = %d)\n", MAX_FUNC_SIZE);
-    printf("Note, function cannot be exponential, logrithmic: \n");
-    scanf("%s", function);
+    printf("\nPlease input the function you want to integrate (max chars = %d)\n", MAX_FUNC_SIZE);
+    //get function with whitespace
+    scanf("%[^\n]s", function);
 
     printf("Please input the number of intervals (N): \n");
     scanf("%d", &N);
@@ -176,10 +216,10 @@ int main()
     double deltaX = (double)(b-a) / (double)N;
     
     // simpsons rule or trapezoidal
-    if (choice == 1) 
+    if (choice == '1') 
     {
         double sum = simpsonsApprox(N, b, a, function, deltaX); 
-        printf("Aproximation of the integral from %d-%d of %s = %f\n", a, b, function, sum);
+        printf("Simpsons Aproximation of the integral from %d-%d of %s = %f\n", a, b, function, sum);
     }
     else 
     {
